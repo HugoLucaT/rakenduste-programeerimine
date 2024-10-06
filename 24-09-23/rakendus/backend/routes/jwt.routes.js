2b5c0df77c2995b6b1c6533de70700513c4f36af
Hugo Luca Tigane <146342693+HugoLucaT@users.noreply.github.com>
2024-10-06 10:49:18 +0300

    feat: jwt

--- 24-09-23/rakendus/backend/routes/jwt.routes.js
+++ 24-09-23/rakendus/backend/routes/jwt.routes.js
@@ -6,11 +6,27 @@ router.get("/", (req, res) => {
   const token = jwt.sign({ name: "Hugo" }, "shh");
 });
 
+router.get("/kodutoo", (req, res) => {
+  const token = jwt.sign(req.body, "shh");
+
+  res.send(token);
+});
+
 const postMiddleware = (req, res, next) => {
   next();
 };
 
 router.post("/", postMiddleware, (req, res) => {
+  const { token } = req.body;
+
+  jwt.verify(token, "shh", function (err, decoded) {
+    if (err) return res.send(false);
+    console.log(decoded);
+    res.send(true);
+  });
+});
+
+router.post("/kodutoo", (req, res) => {
   const { token } = req.body;
 
   jwt.verify(token, "shh", function (err, decoded) {
